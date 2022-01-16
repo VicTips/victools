9d78af2034a5c0a9e9786be231705f9fb281fe76
victips <torvicarias@gmail.com>
2022-01-16 18:14:23 -0500

    Se crea clase Rows con métodos para cuotas fijas, abonos constantes y gradientes lineales crecientes

---
+++ src/services/Rows.js
@@ -0,0 +1,156 @@
+class Rows {
+  constructor(loanAmount, nPer, intRate) {
+    this.loanAmount = loanAmount;
+    this.nPer = nPer;
+    this.intRate = intRate;
+  }
+
+  pmtFixed() {
+    let payment = "";
+    if (this.loanAmount && this.nPer && this.intRate !== "") {
+      payment =
+        (this.loanAmount *
+          (1 + this.intRate / 100) ** this.nPer *
+          this.intRate) /
+        100 /
+        ((1 + this.intRate / 100) ** this.nPer - 1);
+    }
+
+    let rows = [
+      {
+        period: 0,
+        payment: 0,
+        interest: 0,
+        principal: 0,
+        balance: this.loanAmount,
+      },
+    ];
+    if (this.loanAmount && this.nPer && this.intRate !== "") {
+      for (let period = 1; period <= this.nPer; period++) {
+        rows.push({
+          period: period,
+          payment: payment,
+          interest:
+            ((this.loanAmount * (1 + this.intRate / 100) ** (period - 1) -
+              (payment * ((1 + this.intRate / 100) ** (period - 1) - 1)) /
+                (this.intRate / 100)) *
+              this.intRate) /
+            100,
+          principal:
+            payment -
+            ((this.loanAmount * (1 + this.intRate / 100) ** (period - 1) -
+              (payment * ((1 + this.intRate / 100) ** (period - 1) - 1)) /
+                (this.intRate / 100)) *
+              this.intRate) /
+              100,
+          balance:
+            this.loanAmount * (1 + this.intRate / 100) ** period -
+            (payment * ((1 + this.intRate / 100) ** period - 1)) /
+              (this.intRate / 100),
+        });
+      }
+    }
+    return rows;
+  }
+
+  pmtVariable() {
+    let principal = "";
+    if (this.loanAmount && this.nPer && this.intRate !== "") {
+      principal = this.loanAmount / this.nPer;
+    }
+
+    let rows = [
+      {
+        period: 0,
+        payment: 0,
+        interest: 0,
+        principal: 0,
+        balance: this.loanAmount,
+      },
+    ];
+    if (this.loanAmount && this.nPer && this.intRate !== "") {
+      for (let period = 1; period <= this.nPer; period++) {
+        rows.push({
+          period: period,
+          payment:
+            principal +
+            ((this.loanAmount - (this.loanAmount / this.nPer) * (period - 1)) *
+              this.intRate) /
+              100,
+          interest:
+            ((this.loanAmount - (this.loanAmount / this.nPer) * (period - 1)) *
+              this.intRate) /
+            100,
+          principal: principal,
+          balance: this.loanAmount - (this.loanAmount / this.nPer) * period,
+        });
+      }
+    }
+    return rows;
+  }
+
+  linearGrowth(gradient) {
+    let payment = "";
+    if (this.loanAmount && this.nPer && this.intRate !== "") {
+      payment =
+        (this.loanAmount *
+          ((this.intRate / 100) * (1 + this.intRate / 100) ** this.nPer)) /
+          ((1 + this.intRate / 100) ** this.nPer - 1) -
+        gradient *
+          (1 / (this.intRate / 100) -
+            this.nPer / (1 + (this.intRate / 100) ** this.nPer - 1));
+    }
+
+    let rows = [
+      {
+        period: 0,
+        payment: 0,
+        interest: 0,
+        principal: 0,
+        balance: this.loanAmount,
+      },
+    ];
+    if (this.loanAmount && this.nPer && this.intRate !== "") {
+      for (let period = 1; period <= this.nPer; period++) {
+        rows.push({
+          period: period,
+          payment: payment + gradient * period,
+          interest:
+            this.loan * (1 + (this.intRate / 100) ** (period - 1)) -
+            ((payment * ((1 + this.intRate / 100) ** (period - 1) - 1)) /
+              (this.intRate / 100) +
+              (gradient *
+                (1 / (this.intRate / 100) -
+                  (period - 1) /
+                    (1 + (this.intRate / 100) ** (period - 1) - 1)) *
+                ((1 + this.intRate / 100) ** (period - 1) - 1)) /
+                (this.intRate / 100)),
+          principal:
+            payment +
+            gradient * period -
+            (this.loan * (1 + (this.intRate / 100) ** (period - 1)) -
+              ((payment * ((1 + this.intRate / 100) ** (period - 1) - 1)) /
+                (this.intRate / 100) +
+                (gradient *
+                  (1 / (this.intRate / 100) -
+                    (period - 1) /
+                      (1 + (this.intRate / 100) ** (period - 1) - 1)) *
+                  ((1 + this.intRate / 100) ** (period - 1) - 1)) /
+                  (this.intRate / 100))),
+          balance:
+            this.loan * (1 + (this.intRate / 100) ** period) -
+            ((payment * ((1 + this.intRate / 100) ** period - 1)) /
+              (this.intRate / 100) +
+              (gradient *
+                (1 / (this.intRate / 100) -
+                  period / (1 + (this.intRate / 100) ** period - 1)) *
+                ((1 + this.intRate / 100) ** period - 1)) /
+                (this.intRate / 100)),
+        });
+      }
+    }
+    return rows;
+  }
+}
+
+export default Rows;
