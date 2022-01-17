6a893263b50169a0714927e13b904f9e6cf73109
victips <torvicarias@gmail.com>
2022-01-17 13:52:32 -0500

    Se remueven los tabs de la página de Tablas

--- src/pages/Tables.js
+++ src/pages/Tables.js
@@ -1,85 +1,11 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
+import React from 'react'
 
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
+const Tables = () => {
   return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
+    <div>
+      <h1>Tables</h1>
     </div>
-  );
+  )
 }
 
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-}
-
-export default function Tables() {
-  const [value, setValue] = useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  return (
-    <Box sx={{ width: "100%" }}>
-      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          aria-label="types"
-          variant="scrollable"
-        >
-          <Tab label="Cuota Fija" {...a11yProps(0)} />
-          <Tab label="Abono Constante" {...a11yProps(1)} />
-          <Tab label="Linear Creciente" {...a11yProps(2)} />
-          <Tab label="Linear Decreciente" {...a11yProps(3)} />
-          <Tab label="Exponencial Creciente" {...a11yProps(4)} />
-          <Tab label="Exponencial Decreciente" {...a11yProps(5)} />
-        </Tabs>
-      </Box>
-      <TabPanel value={value} index={0}>
-        Item One
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        Item Four
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-        Item Five
-      </TabPanel>
-      <TabPanel value={value} index={5}>
-        Item Six
-      </TabPanel>
-    </Box>
-  );
-}
+export default Tables
