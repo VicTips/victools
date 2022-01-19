4d41151625c2b5a7e1efbef84d9625e105b6fe00
victips <torvicarias@gmail.com>
2022-01-19 10:01:05 -0500

    Se crea método para gradientes lineales decrecientes

--- src/services/Rows.js
+++ src/services/Rows.js
@@ -159,6 +159,77 @@ class Rows {
     }
     return rows;
   }
+
+  linearDecay(gradient) {
+    let payment = "";
+    if (this.loanAmount && this.nPer && this.intRate !== "") {
+      payment =
+        (this.loanAmount *
+          ((this.intRate / 100) * (1 + this.intRate / 100) ** this.nPer)) /
+          ((1 + this.intRate / 100) ** this.nPer - 1) +
+        gradient *
+          (1 / (this.intRate / 100) -
+            this.nPer / ((1 + this.intRate / 100) ** this.nPer - 1));
+    }
+
+    let rows = [
+      {
+        period: 0,
+        payment: 0,
+        interest: 0,
+        principal: 0,
+        balance: this.loanAmount,
+      },
+    ];
+    if (this.loanAmount && this.nPer && this.intRate !== "") {
+      for (let period = 1; period <= this.nPer; period++) {
+        rows.push({
+          period: period,
+          payment: payment + gradient * (period - 1),
+          interest:
+            period === 1
+              ? (this.loanAmount * this.intRate) / 100
+              : ((this.loanAmount * (1 + this.intRate / 100) ** (period - 1) -
+                  ((payment * ((1 + this.intRate / 100) ** (period - 1) - 1)) /
+                    (this.intRate / 100) -
+                    (gradient *
+                      (1 / (this.intRate / 100) -
+                        (period - 1) /
+                          ((1 + this.intRate / 100) ** (period - 1) - 1)) *
+                      ((1 + this.intRate / 100) ** (period - 1) - 1)) /
+                      (this.intRate / 100))) *
+                  this.intRate) /
+                100,
+          principal:
+            payment +
+            gradient * (period - 1) -
+            (period === 1
+              ? (this.loanAmount * this.intRate) / 100
+              : ((this.loanAmount * (1 + this.intRate / 100) ** (period - 1) -
+                  ((payment * ((1 + this.intRate / 100) ** (period - 1) - 1)) /
+                    (this.intRate / 100) -
+                    (gradient *
+                      (1 / (this.intRate / 100) -
+                        (period - 1) /
+                          ((1 + this.intRate / 100) ** (period - 1) - 1)) *
+                      ((1 + this.intRate / 100) ** (period - 1) - 1)) /
+                      (this.intRate / 100))) *
+                  this.intRate) /
+                100),
+          balance:
+            this.loanAmount * (1 + this.intRate / 100) ** period -
+            ((payment * ((1 + this.intRate / 100) ** period - 1)) /
+              (this.intRate / 100) -
+              (gradient *
+                (1 / (this.intRate / 100) -
+                  period / ((1 + this.intRate / 100) ** period - 1)) *
+                ((1 + this.intRate / 100) ** period - 1)) /
+                (this.intRate / 100)),
+        });
+      }
+    }
+    return rows;
+  }
 }
 
 export default Rows;
